25e9feb500c33f889f6767c9e6b78dd67257c8bf
malek <m.darwish@salam.sa>
2024-08-31 19:51:43 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import data from "./data";
+
+const App = () => {
+  const [people, setPeople] = useState(data);
+  return (
+    <>
+      <main>
+        
+      </main>
+    </>
+  );
+};
+export default App;
